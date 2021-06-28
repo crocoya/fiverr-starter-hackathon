a1deeca0c22d995ffd63f66b3da137c4bc3902eb
margotToullier <margottoullier@gmail.com>
2021-06-28 16:09:19 +0200

    finish partie 1 of client view

--- src/component/Client.jsx
+++ src/component/Client.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import './Client.css'
+import Search from './Search'
 
 function Client() {
   return (
     <div className="layout-service .discover-fiverr">
-      <div></div>
-      <p>Bienvenue sur Fiverr ! Souhaitez-vous découvrir Fiverr en français ?</p>
-      <span className="discover-link">C'est parti !</span>
+      <div className="intro">
+        <p>Bienvenue sur Fiverr ! Souhaitez-vous découvrir Fiverr en français ?</p>
+        <span className="discover-link">C'est parti !</span>
+      </div>
       <header>
         <div className='logo'>
           <i className='bx bxs-drink' />
           <a href='/'>
             fiverr<span className='dot'>.</span>
           </a>
+        </div>
+        <div className="search">
+        <Search />
         </div>
         <div className='links'>
           <ul>
