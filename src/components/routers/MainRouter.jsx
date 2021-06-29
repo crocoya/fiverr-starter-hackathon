477a972908e8af3354360a9596ee98040ec5d556
Crocoya <yassine.tababi@outlook.com>
2021-06-29 22:10:25 +0200

    add notifications

--- src/components/routers/MainRouter.jsx
+++ src/components/routers/MainRouter.jsx
@@ -6,19 +6,20 @@ import MarketPage from '../pages/MarketPage';
 import DashboardPro from '../pages/DashboardPro';
 import Client from '../commons/Client';
 import ClientProject from '../pages/ClientProject';
+
 export default function MainRouter() {
   return (
     <>
       <Router>
         <Switch>
-          <Route exact path="/" component={HomePage} />
+          <Route exact path='/' component={HomePage} />
 
-          <Route exact path="/marketplace-freelances" component={MarketPage} />
+          <Route exact path='/marketplace-freelances' component={MarketPage} />
 
-          <Route exact path="/dashboard-freelances" component={DashboardPro} />
-          <Route exact path="/clientProject" component={ClientProject} />
-          <Route exact path="/marketplace-clients" component={Client} />
-          <Route exact path="/game" component={GamePage} />
+          <Route exact path='/dashboard-freelances' component={DashboardPro} />
+          <Route exact path='/clientProject' component={ClientProject} />
+          <Route exact path='/marketplace-clients' component={Client} />
+          <Route exact path='/game' component={GamePage} />
         </Switch>
       </Router>
     </>
