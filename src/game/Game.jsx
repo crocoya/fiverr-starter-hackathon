ba1227d688089c0a04a7c3abe90a5b3f90b23cc0
Antoine <antoinepoppe@hotmail.fr>
2021-06-29 10:49:52 +0200

    mieux

--- src/game/Game.jsx
+++ src/game/Game.jsx
@@ -3,22 +3,27 @@ import Cardboard from './Cardboard';
 import './Game.css';
 
 function Game() {
-  const [playing, isPlaying] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="Game">
-      <p>
-        This is a short game to help you to find projects as a new freelance on
-        Fiverr : The most points you got, the more likely you are to get a
-        project of your choice !
-      </p>
-      <p>
-        You can choose to stop playing as soon as you're happy with your score,
-        or to continue drawing cards. But be careful, if you go over 12, you
-        loose.
-      </p>
-
-      {!isPlaying ? <button>Start the game !</button> : <Cardboard />}
+      {!isPlaying ? (
+        <div>
+          <p>
+            This is a short game to help you to find projects as a new freelance
+            on Fiverr : The most points you got, the more likely you are to get
+            a project of your choice !
+          </p>
+          <p>
+            You can choose to stop playing as soon as you're happy with your
+            score, or to continue drawing cards. But be careful, if you go over
+            12, you loose.
+          </p>
+          <button onClick={() => setIsPlaying(true)}>Start the game !</button>
+        </div>
+      ) : (
+        <Cardboard />
+      )}
     </div>
   );
 }
