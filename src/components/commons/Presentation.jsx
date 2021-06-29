f831bd9296a9acb79220bea088f65ee386fb884d
Crocoya <yassine.tababi@outlook.com>
2021-06-30 01:19:12 +0200

    add animation

--- src/components/commons/Presentation.jsx
+++ src/components/commons/Presentation.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { init } from 'ityped';
+
 import './style/Presentation.css';
 
 export default function Presentation() {
+  const textRef = React.useRef();
+
+  React.useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,
+      strings: ['Freelance'],
+    });
+  }, []);
   return (
     <div className='container-slide'>
       <div className='container-horizontal'>
@@ -13,27 +24,33 @@ export default function Presentation() {
           <span>Slide 2</span>
         </div>
         <div className='slide three'>
-          <div class='container-s ui action left icon input '>
-            <i
-              aria-hidden='true'
-              class='search icon'
-              style={{ fontSize: '16px' }}
-            />
-            <input type='text' placeholder='Try "developper website"' />
-            <Link to='/marketplace-freelances'>
-              <button
-                class='btn-recherche ui button'
-                style={{
-                  backgroundColor: '#1dbf73',
-                  color: 'white',
-                  fontSize: '16px',
-                  width: '100%',
-                  height: '100%',
-                }}
-              >
-                Search
-              </button>
-            </Link>
+          <div className='container-banner'>
+            <span className='title-banner'>
+              Find the perfect <i ref={textRef}></i>
+            </span>{' '}
+            <span className='subtitle-banner'>services for your business</span>
+            <div className='container-s ui action left icon input '>
+              <i
+                aria-hidden='true'
+                className='search icon'
+                style={{ fontSize: '16px' }}
+              />
+              <input type='text' placeholder='Try "developper website"' />
+              <Link to='/marketplace-freelances'>
+                <button
+                  className='btn-recherche ui button'
+                  style={{
+                    backgroundColor: '#1dbf73',
+                    color: 'white',
+                    fontSize: '16px',
+                    width: '100%',
+                    height: '100%',
+                  }}
+                >
+                  Search
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
