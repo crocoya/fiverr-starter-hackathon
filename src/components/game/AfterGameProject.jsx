2e3ad9f3fd3c7f09150e97575a3eecf3a9c535e3
Arthur Sallé <arthursalle.pro@gmail.com>
2021-06-30 10:51:06 +0200

    style

--- src/components/game/AfterGameProject.jsx
+++ src/components/game/AfterGameProject.jsx
@@ -3,6 +3,7 @@ import './AfterGameProject.css';
 
 function AfterGameProject({
   id,
+  img,
   title,
   theme,
   description,
@@ -12,19 +13,39 @@ function AfterGameProject({
 }) {
   return (
     <div
-      className="projectContainer"
+      className='projectContainer'
       onClick={() => {
         if (id === 1) {
           click();
         }
       }}
     >
-      <h3>{title}</h3>
+      {/* <h3>{title}</h3>
       <p>{theme}</p>
       <p>{description}</p>
       <div>
         <p>{tarif}</p>
         <p>{delai}</p>
+      </div> */}
+      <div className='nav-item cardito'>
+        <img src={img} alt='' />
+        <div className='nav-content'>
+          <h3>{title}</h3>
+          <p>{theme}</p>
+          <p>{description}</p>
+          {/* <div className='content-note'>
+            <span>
+              <i className='bx bxs-star' />
+              &nbsp;5.0
+            </span>
+            &nbsp; (617)
+          </div> */}
+          <div className='content-price'>
+            <p>{tarif}</p>
+            <br />
+            <p>{delai}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/pages/AfterGame.jsx
+++ src/components/pages/AfterGame.jsx
@@ -10,52 +10,58 @@ function AfterGame() {
   const [projectsToShow, setProjectsToShow] = useState([]);
   const [messageToDisplay, setMessageToDisplay] = useState(null);
   const [projectChoosed, setProjectChoosed] = useState(false);
+
   const projects = [
     {
       id: 1,
+      img: 'https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/171770678/original/d8c950e18b81ed00b8dea152dba7fcc5f8f062be.jpg',
       title: 'Design and build a custom wix website',
-      theme: 'Web design',
+      theme: 'Web Design',
       description:
         'I need an e-commerce site that is properly referenced to rank well on Google and also that the mobile interface is optimal so that the website looks just as amazing on all devices.',
-      tarif: '17,63€',
+      tarif: '17,63 €',
       delai: '2 jours',
       score: 12,
     },
     {
       id: 2,
+      img: 'https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/3737290/original/a29919f68690402846df3c0c8a9848b8550d55ea.png',
       title: 'Email banner, header, footer and email signature',
       theme: 'Design',
       description:
         'I am looking for a designer to create a banner, a footer and an email signature for our website.',
-      tarif: '8,8',
+      tarif: '8,80 €',
       delai: '1 jour',
     },
     {
       id: 3,
       title: 'Wordpress website design, responsive wordpress website',
-      theme: 'Design & responsive',
+      img: 'https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/157750073/original/006c1cd999f95a6f6847455a687ad8fb63be1738.jpg',
+      theme: 'Design & Responsive',
       description:
         'Creating a new modern looking wordpress website or customising and reorganising the existing wordpress.',
-      tarif: '44,07',
+      tarif: '44,50 €',
       delai: '3 jours',
       score: 8,
     },
     {
       id: 4,
-      title: 'Create unique custom icons',
-      theme: 'Custom icon',
+      title: 'Create unique custom icons with fancy colors',
+      img: 'https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/166530150/original/9b7d04f2c3f16ed6171a39b7d30a17994021c648.jpg',
+      theme: 'Custom Icon',
       description:
         'I am looking for an expert illustrator to create beautiful, detailed illustrations, including vector illustrations of custom icons.',
-      tarif: '35',
+      tarif: '35,30 €',
       delai: '7 jours',
     },
     {
       id: 5,
       title: 'Design professional email newsletters',
+      img: 'https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/120179950/original/82e1dbbcbb8e94b38bff75ca4fc3dfd83641dd60.jpg',
       theme: 'Email',
       description:
         'Creation of customisable HTML templates for email alerts. MARKETING PLATFORM templates must be 100% customisable in the marketing platform.',
-      tarif: '22',
+      tarif: '22 €',
       delai: '3 jours',
       score: 4,
     },
@@ -66,27 +72,27 @@ function AfterGame() {
     if (score > 10 && score < 13) {
       setProjectsToShow(projects);
       setMessageToDisplay(
-        `You got ${score} points. Therefore, you can choose between those 5 projects !`
+        `🎉 You got ${score} points. Therefore, you can choose between those 5 projects! 🎉`
       );
     } else if (score >= 8 && score <= 10) {
       setProjectsToShow(projects.splice(0, 4));
       setMessageToDisplay(
-        `You got ${score} points. Therefore, you can choose between those 4 projects !`
+        `🎉 You got ${score} points. Therefore, you can choose between those 4 projects! 🎉`
       );
     } else if (score >= 5 && score < 8) {
       setProjectsToShow(projects.splice(0, 3));
       setMessageToDisplay(
-        `You got ${score} points. Therefore, you can choose between those 3 projects !`
+        `🎉 You got ${score} points. Therefore, you can choose between those 3 projects! 🎉`
       );
     } else if (score >= 2 && score < 5) {
       setProjectsToShow(projects.splice(0, 2));
       setMessageToDisplay(
-        `You got ${score} points. Therefore, you only can choose between those 2 projects !`
+        `🎉 You got ${score} points. Therefore, you only can choose between those 2 projects! 🎉`
       );
     } else {
       setProjectsToShow(projects.splice(0, 1));
       setMessageToDisplay(
-        `You got ${score} points. This is the last project left. But be sure you will do better next time ! (We'll provide you a chance bonus if you accept this project !)  `
+        `🎉 You got ${score} points. This is the last project left. But be sure you will do better next time ! (We'll provide you a chance bonus if you accept this project!) 🎉`
       );
     }
   }, []);
@@ -103,13 +109,13 @@ function AfterGame() {
   return (
     <>
       <header>
-        <div className="logo">
+        <div className='logo'>
           {/* <i className='bx bxs-drink' /> */}
-          <a href="/">
-            fiverr<span className="dot">.</span>
+          <a href='/'>
+            fiverr<span className='dot'>.</span>
           </a>
         </div>
-        <div className="links">
+        <div className='links'>
           <ul>
             <li>Fiverr Pro</li>
             <li>Explore</li>
@@ -119,14 +125,17 @@ function AfterGame() {
         </div>
       </header>
       <main>
-        <div className="container-aftergame">
-          {!projectChoosed ? <p>{messageToDisplay}</p> : null}
-          <div className="projectsContainer">
+        <div className='container-aftergame'>
+          {!projectChoosed ? (
+            <p className='text-score'>{messageToDisplay}</p>
+          ) : null}
+          <div className='projectsContainer'>
             {!projectChoosed ? (
               projectsToShow.map((project) => (
                 <AfterGameProject
                   id={project.id}
                   key={project.id}
+                  img={project.img}
                   title={project.title}
                   theme={project.theme}
                   // description={project.description}
