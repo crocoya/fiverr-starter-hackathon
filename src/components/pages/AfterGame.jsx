adff67fb799a533e6b30bd30c7254d1e870ccbb2
Antoine <antoinepoppe@hotmail.fr>
2021-06-30 09:14:32 +0200

    super

--- src/components/pages/AfterGame.jsx
+++ src/components/pages/AfterGame.jsx
@@ -19,14 +19,16 @@ function AfterGame() {
         'I need an e-commerce site that is properly referenced to rank well on Google and also that the mobile interface is optimal so that the website looks just as amazing on all devices.',
       tarif: '17,63€',
       delai: '2 jours',
+      score: 12,
     },
     {
       id: 2,
-      title: 'Modern UI UX design for your website',
-      theme: '',
-      description: '',
-      tarif: '',
-      delai: ' jours',
+      title: 'Email banner, header, footer and email signature',
+      theme: 'Design',
+      description:
+        'I am looking for a designer to create a banner, a footer and an email signature for our website.',
+      tarif: '8,8',
+      delai: '1 jour',
     },
     {
       id: 3,
@@ -36,22 +38,26 @@ function AfterGame() {
         'Creating a new modern looking wordpress website or customising and reorganising the existing wordpress.',
       tarif: '44,07',
       delai: '3 jours',
+      score: 8,
     },
     {
       id: 4,
-      title: 'Design modern web and mobile banner ads',
-      theme: '',
-      description: '',
-      tarif: '',
-      delai: ' jours',
+      title: 'Create unique custom icons',
+      theme: 'Custom icon',
+      description:
+        'I am looking for an expert illustrator to create beautiful, detailed illustrations, including vector illustrations of custom icons.',
+      tarif: '35',
+      delai: '7 jours',
     },
     {
       id: 5,
-      title: 'A desktop interface',
-      theme: '',
-      description: '',
-      tarif: '',
-      delai: ' jours',
+      title: 'Design professional email newsletters',
+      theme: 'Email',
+      description:
+        'Creation of customisable HTML templates for email alerts. MARKETING PLATFORM templates must be 100% customisable in the marketing platform.',
+      tarif: '22',
+      delai: '3 jours',
+      score: 4,
     },
   ];
 
