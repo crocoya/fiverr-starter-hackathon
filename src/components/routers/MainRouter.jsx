9d00b0ec15d77b9b80217c1e4c74ad48e125cfd1
Antoine <antoinepoppe@hotmail.fr>
2021-06-30 11:55:22 +0200

    lallaa

--- src/components/routers/MainRouter.jsx
+++ src/components/routers/MainRouter.jsx
@@ -4,7 +4,6 @@ import HomePage from '../pages/HomePage';
 import GamePage from '../pages/GamePage';
 import MarketPage from '../pages/MarketPage';
 import DashboardPro from '../pages/DashboardPro';
-import Client from '../commons/Client';
 import ClientProject from '../pages/ClientProject';
 import AfterGame from '../pages/AfterGame';
 import Thanks from '../pages/Thanks';
@@ -21,7 +20,6 @@ export default function MainRouter() {
 
           <Route exact path="/dashboard-freelances" component={DashboardPro} />
           <Route exact path="/clientProject" component={ClientProject} />
-          <Route exact path="/marketplace-clients" component={Client} />
           <Route exact path="/game" component={GamePage} />
           <Route exact path="/afterGame" component={AfterGame} />
           <Route exact path="/gamefinish" component={AfterGame} />
